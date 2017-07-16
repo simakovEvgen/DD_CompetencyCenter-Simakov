4f551f8b9b619fdf6c6df6fb3d7b213aec55445e
simakovEvgen <simakov.evgens@gmail.com>
2017-07-16 14:34:29 +0300

    done

--- ReactJS/lesson_23/ClassWork/task1/app/build/bundle.js
+++ ReactJS/lesson_23/ClassWork/task1/app/build/bundle.js
@@ -1,107 +1,95 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
+/******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/ 		}
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
+/******/ 		module.loaded = true;
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-/*
-* Class Work
-*
-* ### Задача 1
-*
-* Создайте папку app. В ней создайте файл index.html и папку src. В папке src создайте 2 файла:
-* файл app.js с произвольным кодом
-* файл main.js, который загрузит код файла app.js
-* С помощью webpack свяжите эти страницы в файл bundle.js и подключите его к странице index.html.
-*/
+	'use strict';
 
-class Hello {
-    constructor(){
-        this.state = {str: "Hello!"}
-    }
-    sayHi(){
-        console.log(this.state.str)
-    }
-}
+	var Hello = __webpack_require__(1);
+	var e = new Hello();
 
-module.exports = Hello;
+	e.sayHi();
 
-
-/***/ }),
+/***/ },
 /* 1 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
+	"use strict";
 
-var Hello = __webpack_require__(0);
-var e = new Hello();
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-e.sayHi();
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-/***/ })
+	/*
+	* Class Work
+	*
+	* ### Задача 1
+	*
+	* Создайте папку app. В ней создайте файл index.html и папку src. В папке src создайте 2 файла:
+	* файл app.js с произвольным кодом
+	* файл main.js, который загрузит код файла app.js
+	* С помощью webpack свяжите эти страницы в файл bundle.js и подключите его к странице index.html.
+	*/
+
+	var Hello = function () {
+	    function Hello() {
+	        _classCallCheck(this, Hello);
+
+	        this.state = { str: "Hello!" };
+	    }
+
+	    _createClass(Hello, [{
+	        key: "sayHi",
+	        value: function sayHi() {
+	            console.log(this.state.str);
+	        }
+	    }]);
+
+	    return Hello;
+	}();
+
+	module.exports = Hello;
+
+/***/ }
 /******/ ]);
